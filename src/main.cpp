4d12a9d172c9e2954e5107516fa9943105e5b364
Dustin Ivander <ivander1908@gmail.com>
2026-01-10 00:42:40 +0700

    Test window scaling and full-screen mode

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,29 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 
+sf::View getLetterboxView(sf::View view, int winW, int winH)
+{
+    float windowRatio = (float)winW / (float)winH;
+    float viewRatio = view.getSize().x / view.getSize().y;
+
+    float sizeX = 1.f, sizeY = 1.f;
+    float posX = 0.f, posY = 0.f;
+
+    if (windowRatio > viewRatio)
+    {
+        sizeX = viewRatio / windowRatio;
+        posX = (1.f - sizeX) / 2.f;
+    }
+    else
+    {
+        sizeY = windowRatio / viewRatio;
+        posY = (1.f - sizeY) / 2.f;
+    }
+
+    view.setViewport({posX, posY, sizeX, sizeY});
+    return view;
+}
+
 enum MenuItem
 {
     PLAY,
@@ -10,8 +33,51 @@ enum MenuItem
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Controller Menu");
+    constexpr unsigned GAME_W = 800;
+    constexpr unsigned GAME_H = 600;
+
+    bool fullscreen = false;
+
+    sf::VideoMode windowedMode(GAME_W, GAME_H);
+    sf::VideoMode fullscreenMode = sf::VideoMode::getDesktopMode();
+
+    sf::RenderWindow window(
+        windowedMode,
+        "SFML Game",
+        sf::Style::Default);
+
+    sf::View gameView(sf::FloatRect(0, 0, GAME_W, GAME_H));
+    window.setView(gameView);
+
     window.setFramerateLimit(60);
+
+    auto recreateWindow = [&](bool fs)
+    {
+        window.close();
+
+        if (fs)
+        {
+            window.create(
+                fullscreenMode,
+                "SFML Game",
+                sf::Style::Fullscreen);
+        }
+        else
+        {
+            window.create(
+                windowedMode,
+                "SFML Game",
+                sf::Style::Default);
+        }
+
+        gameView = getLetterboxView(
+            sf::View(sf::FloatRect(0, 0, GAME_W, GAME_H)),
+            window.getSize().x,
+            window.getSize().y);
+
+        window.setView(gameView);
+        window.setVerticalSyncEnabled(true);
+    };
 
     // ---- Load font ----
     sf::Font font;
@@ -48,6 +114,22 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (event.type == sf::Event::KeyPressed &&
+                event.key.code == sf::Keyboard::F11)
+            {
+                fullscreen = !fullscreen;
+                recreateWindow(fullscreen);
+            }
+
+            if (event.type == sf::Event::Resized)
+            {
+                gameView = getLetterboxView(
+                    gameView,
+                    event.size.width,
+                    event.size.height);
+                window.setView(gameView);
+            }
 
             // ---- Keyboard navigation ----
             if (event.type == sf::Event::KeyPressed)
